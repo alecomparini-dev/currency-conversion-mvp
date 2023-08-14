469b90c1deae44eb8e85e2df48f098723838349e
Comparini <alecomparini.dev@gmail.com>
2023-08-14 10:09:59 -0300

    Initial Commit

---
+++ currency-conversion-mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  currency-conversion-mvvm
+//
+//  Created by Alessandro Comparini on 14/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ currency-conversion-mvvmTests/currency_conversion_mvvmTests.swift
@@ -0,0 +1,36 @@
+//
+//  currency_conversion_mvvmTests.swift
+//  currency-conversion-mvvmTests
+//
+//  Created by Alessandro Comparini on 14/08/23.
+//
+
+import XCTest
+@testable import currency_conversion_mvvm
+
+final class currency_conversion_mvvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
