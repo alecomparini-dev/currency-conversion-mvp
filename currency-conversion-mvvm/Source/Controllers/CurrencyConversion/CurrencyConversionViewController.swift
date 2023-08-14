1a75f5247f33cec418b720e7351ca2aa0cb8a124
Comparini <alecomparini.dev@gmail.com>
2023-08-14 15:06:33 -0300

    configure the project start

---
+++ currency-conversion-mvvm/Source/Controllers/CurrencyConversion/CurrencyConversionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CurrencyConversionViewController.swift
+//  currency-conversion-mvvm
+//
+//  Created by Alessandro Comparini on 14/08/23.
+//
+
+import UIKit
+
+class CurrencyConversionViewController: UIViewController {
+    
+    lazy var screen: CurrencyConversionView = {
+        let view = CurrencyConversionView()
+        return view
+    }()
+    
+    override func loadView() {
+        view = screen
+    }
+    
+    
+    
+}
