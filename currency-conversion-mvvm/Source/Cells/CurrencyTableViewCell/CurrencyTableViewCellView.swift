2bdb5ceaa951526fc27f0c0c9c92bf4eb0e45c0f
Comparini <alecomparini.dev@gmail.com>
2023-08-17 14:31:06 -0300

    create tableview and tableview cell

---
+++ currency-conversion-mvvm/Source/Cells/CurrencyTableViewCell/CurrencyTableViewCellView.swift
@@ -0,0 +1,22 @@
+//
+//  CurrencyTableViewCellView.swift
+//  currency-conversion-mvvm
+//
+//  Created by Alessandro Comparini on 17/08/23.
+//
+
+import UIKit
+
+class CurrencyTableViewCellView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
