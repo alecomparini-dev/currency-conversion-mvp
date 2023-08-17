23fcc7f40093e0d66e92abb880e18790b8c49c82
Comparini <alecomparini.dev@gmail.com>
2023-08-17 16:57:13 -0300

    create sort view

--- currency-conversion-mvvm/Source/ViewControllers/SearchCurrencies/SearchCurrenciesViewController.swift
+++ currency-conversion-mvvm/Source/ViewControllers/SearchCurrencies/SearchCurrenciesViewController.swift
@@ -7,9 +7,15 @@
 
 import UIKit
 
+
+//  MARK: - PROTOCOL
+
 protocol SearchCurrenciesViewControllerCoordinator: AnyObject {
     func goToCurrencyConversionVC()
 }
+
+
+//  MARK: - CLASS
 
 class SearchCurrenciesViewController: UIViewController, ViewControllerCoordinator {
     weak var coordinator: SearchCurrenciesViewControllerCoordinator?
@@ -30,22 +36,82 @@ class SearchCurrenciesViewController: UIViewController, ViewControllerCoordinato
     
     
 //  MARK: - PRIVATE AREA
+    
     private func configure() {
         hideKeyboardOnTap()
-        configDelegate()
+        configureDelegates()
     }
     
-    private func configDelegate() {
+    private func configureDelegates() {
+        configSearchCurrenciesViewDelegate()
+        configSearchBarDelegate()
+        configTableViewDelegate()
+    }
+    
+    private func configSearchCurrenciesViewDelegate() {
         screen.delegate = self
+    }
+    
+    private func configSearchBarDelegate() {
+        screen.searchBar.delegate = self
+    }
+    
+    private func configTableViewDelegate() {
+        screen.tableView.delegate = self
+        screen.tableView.dataSource = self
     }
     
 }
 
 
+
 //  MARK: - EXTENSION SearchCurrenciesViewDelegate
+
 extension SearchCurrenciesViewController: SearchCurrenciesViewDelegate {
     func backPageButtonTapped() {
         coordinator?.goToCurrencyConversionVC()
     }
     
 }
+
+
+//  MARK: - EXTENSION UISearchBarDelegate
+
+extension SearchCurrenciesViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+}
+
+
+//  MARK: - EXTENSION UITableViewDelegate
+
+extension SearchCurrenciesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+}
+
+
+//  MARK: - EXTENSION UITableViewDataSource
+
+extension SearchCurrenciesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
