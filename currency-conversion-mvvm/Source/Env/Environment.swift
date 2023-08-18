c3f38bc1c1637cfc63757a83f3e6900126a88c52
Comparini <alecomparini.dev@gmail.com>
2023-08-18 11:49:38 -0300

    create env files and rename project

---
+++ currency-conversion-mvvm/Source/Env/Environment.swift
@@ -0,0 +1,17 @@
+//
+//  Environment.swift
+//  currency-conversion-mvvm
+//
+//  Created by Alessandro Comparini on 18/08/23.
+//
+
+import Foundation
+
+final class Environment {
+    
+    enum Variables: String {
+        case apiBaseUrl = "API_BASE_URL"
+        case typeFetch = "TYPE_FETCH"
+    }
+    
+}
