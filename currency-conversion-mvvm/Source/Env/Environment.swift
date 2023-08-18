72f2caeb8c4f081c7fb584fa07e1c820c64ecb31
Comparini <alecomparini.dev@gmail.com>
2023-08-18 14:20:05 -0300

    chore: Configure project for calls env files by environment

--- currency-conversion-mvvm/Source/Env/Environment.swift
+++ currency-conversion-mvvm/Source/Env/Environment.swift
@@ -9,9 +9,17 @@ import Foundation
 
 final class Environment {
     
+    private static let ENV = "ENV"
+    
     enum Variables: String {
         case apiBaseUrl = "API_BASE_URL"
+        case accessKey = "ACCESS_KEY"
         case typeFetch = "TYPE_FETCH"
+    }
+    
+    static func variable(_ key: Environment.Variables) -> String {
+        let enviroment = Bundle.main.infoDictionary![ENV] as! [String: Any]
+        return enviroment[key.rawValue] as! String
     }
     
 }
