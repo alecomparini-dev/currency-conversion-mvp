22698ff6982a380d118b6b78fa63a0e94188fc9b
Comparini <alecomparini.dev@gmail.com>
2023-08-20 11:06:58 -0300

    create selected currency to convertion

--- currency-conversion-mvvm/Source/UI/ViewControllers/CurrencyConversion/CurrencyConversionViewController.swift
+++ currency-conversion-mvvm/Source/UI/ViewControllers/CurrencyConversion/CurrencyConversionViewController.swift
@@ -7,12 +7,18 @@
 
 import UIKit
 
+
+//  MARK: - PROTOCOL COORDINATOR
 protocol CurrencyConversionViewControllerCoordinator: AnyObject {
     func goToSearchCurrenciesVC()
 }
 
+
+//  MARK: - CLASS
+
 class CurrencyConversionViewController: UIViewController, ViewControllerCoordinator {
     weak var coordinator: CurrencyConversionViewControllerCoordinator?
+    private var tap: (currencyOf: UITapGestureRecognizer?, currencyTo: UITapGestureRecognizer?)
     
     lazy var screen: CurrencyConversionView = {
         let view = CurrencyConversionView()
@@ -28,15 +34,22 @@ class CurrencyConversionViewController: UIViewController, ViewControllerCoordina
         configure()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        addTapGestureCurrency()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        removeTapGesture()
+    }
+    
     
 //  MARK: - PRIVATE AREA
     
     private func configure() {
         hideKeyboardOnTap()
         configDelegate()
-        
-        
-        
         /*
          var dateAPI = Date(timeIntervalSince1970: jsonTime as! Double)
          let dateFormatter = DateFormatter()
@@ -51,12 +64,55 @@ class CurrencyConversionViewController: UIViewController, ViewControllerCoordina
         screen.delegate = self
     }
     
+    private func addTapGestureCurrency() {
+        addTapGestureCurrencyOf()
+        addTapGestureCurrencyTo()
+    }
+    
+    private func addTapGestureCurrencyOf() {
+        tap.currencyOf = makeTapGesture(#selector(currencyOfTapped))
+        guard let tapCurrencyOf = tap.currencyOf else {return}
+        addTapGesture(tap: tapCurrencyOf, screen.currencyOf)
+    }
+
+    private func addTapGestureCurrencyTo() {
+        tap.currencyTo = makeTapGesture(#selector(currencyToTapped))
+        guard let tapCurrencyTo = tap.currencyTo else {return}
+        addTapGesture(tap: tapCurrencyTo, screen.currencyTo)
+    }
+
+    private func addTapGesture(tap: UITapGestureRecognizer, _ currencyView: CurrencyView) {
+        currencyView.addGestureRecognizer(tap)
+    }
+    
+    private func makeTapGesture(_ action: Selector) -> UITapGestureRecognizer {
+        return UITapGestureRecognizer(target: self, action: action)
+    }
+    
+    private func removeTapGesture() {
+        if let tapCurrencyOf = tap.currencyOf, let tapCurrencyTo = tap.currencyTo {
+            screen.currencyOf.removeGestureRecognizer(tapCurrencyOf)
+            screen.currencyTo.removeGestureRecognizer(tapCurrencyTo)
+        }
+    }
+    
+
+    
+//  MARK: - @OBJC AREA
+    @objc private func currencyOfTapped() {
+        currencyOfButtonTapped()
+    }
+    
+    @objc private func currencyToTapped() {
+        currencyToButtonTapped()
+    }
     
 }
 
 
 //  MARK: - EXTENSION CURRENCY CONVERSION VIEW
 extension CurrencyConversionViewController: CurrencyConversionViewDelegate {
+
     func currencyOfButtonTapped() {
         coordinator?.goToSearchCurrenciesVC()
     }
@@ -64,5 +120,10 @@ extension CurrencyConversionViewController: CurrencyConversionViewDelegate {
     func currencyToButtonTapped() {
         coordinator?.goToSearchCurrenciesVC()
     }
-       
+    
+    func invertCurrencyButtonTapped() {
+        print("Invert Currencies")
+    }
+    
 }
+
