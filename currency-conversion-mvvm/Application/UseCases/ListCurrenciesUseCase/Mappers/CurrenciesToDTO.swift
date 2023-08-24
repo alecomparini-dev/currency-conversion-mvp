95ea88a1cbe256597477018283a73343f3260865
Comparini <alecomparini.dev@gmail.com>
2023-08-24 20:31:40 -0300

    rename model Currency

--- currency-conversion-mvvm/Application/UseCases/ListCurrenciesUseCase/Mappers/CurrenciesToDTO.swift
+++ currency-conversion-mvvm/Application/UseCases/ListCurrenciesUseCase/Mappers/CurrenciesToDTO.swift
@@ -11,7 +11,7 @@ import Foundation
 struct CurrenciesMapper {
     static func toListCurrencyResponse(domain: [Currency]) -> [ListCurrencyViewModelResponse] {
         return domain.map { ListCurrencyViewModelResponse(symbol: "",
-                                             title: $0.acronym,
+                                             title: $0.currencyISO,
                                              subTitle: $0.name,
                                              favorite: false) }
     }
