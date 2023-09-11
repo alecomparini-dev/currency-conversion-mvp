1db84bde2bfa0e8e8db5b689e4689bd78e8ed2ff
Comparini <alecomparini.dev@gmail.com>
2023-09-11 14:45:32 -0300

    remove realm

--- Pods/Target Support Files/Pods-currency-clean-architecture-mvp/Pods-currency-clean-architecture-mvp-frameworks.sh
+++ Pods/Target Support Files/Pods-currency-clean-architecture-mvp/Pods-currency-clean-architecture-mvp-frameworks.sh
@@ -178,14 +178,10 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReachabilitySwift/Reachability.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Realm/Realm.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/RealmSwift/RealmSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
